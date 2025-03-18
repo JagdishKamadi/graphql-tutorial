1f13455b237d87fd957feeaec7dbc110bd780fbb
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2025-03-18 08:25:45 +0530

    Updated one to one mapping

---
+++ graphql-demo/src/main/java/com/graphql/graphql_demo/model/Author.java
@@ -0,0 +1,4 @@
+package com.graphql.graphql_demo.model;
+
+public record Author(Integer id, String name, String gender) {
+}
--- graphql-demo/src/main/java/com/graphql/graphql_demo/model/Book.java
+++ graphql-demo/src/main/java/com/graphql/graphql_demo/model/Book.java
@@ -3,12 +3,12 @@ package com.graphql.graphql_demo.model;
 import java.util.Arrays;
 import java.util.List;
 
-public record Book(Integer id, String name, Integer price) {
+public record Book(Integer id, String name, Integer price, Author author) {
 
     private static List<Book> books = Arrays.asList(
-            new Book(1, "Drilling in c", 100),
-            new Book(2, "Java in 60 minutes", 250),
-            new Book(3, "Rich Dad, Poor Dad", 200)
+            new Book(1, "Drilling in c", 100, new Author(1, "Jagdish Kamadi", "Male")),
+            new Book(2, "Java in 60 minutes", 250, new Author(2, "Vedu Shinde", "Female")),
+            new Book(3, "Rich Dad, Poor Dad", 200, new Author(3, "Madhur Bhoyar", "Alpha Male"))
     );
 
     public static List<Book> getAllBook() {
